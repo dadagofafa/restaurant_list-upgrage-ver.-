f9ab00c5fcefec20f1f7a733a0d9103e27cf02ad
dadagofafa <fu6fu62022@gmail.com>
2021-11-06 14:51:40 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,38 +1,25 @@
-// require packages used in the project
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+
 const Restaurant = require('./models/restaurant')
 const restaurantList = require('./restaurant')
 const restaurant = require('./models/restaurant')
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
-
-mongoose.connect('mongodb://localhost/restaurant_list')
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
 app.use(routes)
 
-
-// start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
